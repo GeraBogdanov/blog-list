698d4f9ed2dff92e88d8f972c362adbcc9400e97
Gary Crosby <21145626+GeraBogdanov@users.noreply.github.com>
2024-01-08 18:50:58 +0300

    Add total likes tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   if (blogs) return 1
 }
 
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((total, blog) => { return total = total + blog.likes}, 0)
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
